47ba4868224e0fea5b789316691f2fe43a91fcf8
longj724 <justinlong596@gmail.com>
2024-01-26 23:44:12 -0500

    Pause embeddings when hitting openai rate limit

--- newPipeline/utils/getJobInfoFromGHApplication.js
+++ newPipeline/utils/getJobInfoFromGHApplication.js
@@ -7,6 +7,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports.getJobInfoFromGreenhouseApplications = async () => {
+  let numEmbeddingCalls = 0;
+  let startTime = Date.now();
   const files = fs.readdirSync('./data/applications/gh');
   files.forEach(async (file) => {
     const content = fs.readFileSync(`./data/applications/gh/${file}`);
@@ -16,16 +18,20 @@ module.exports.getJobInfoFromGreenhouseApplications = async () => {
     const jobTitle = $('.app-title').text();
     const location = $('.location').text();
 
-    const applicationDivs = $('#content > div');
+    const applicationDivs = $('#content p, #content span, #content ul');
 
     let applicationText = '';
+    let applicationChunks = [];
     applicationDivs.each(function () {
       const text = $(this).text().trim();
       applicationText += text;
+      if (text.split(' ').length > 7) {
+        applicationChunks.push(text);
+      }
     });
 
     // Put application into db
-    if (companyName != '' && jobTitle != '' && applicationText != '') {
+    if (companyName != '' && jobTitle != '' && applicationChunks.length > 0) {
       const supabase = supabaseClient();
       const { data: applicationData, error: appError } = await supabase
         .from('Applications')
@@ -36,22 +42,44 @@ module.exports.getJobInfoFromGreenhouseApplications = async () => {
           content: applicationText,
         })
         .select('*');
+      console.log('appData is', applicationData);
+      console.log('chunks are', applicationChunks);
 
-      const embeddingResponse = await openAI.embeddings.create({
-        model: 'text-embedding-ada-002',
-        input: applicationText,
-      });
+      if (applicationData !== null) {
+        for (const chunk of applicationChunks) {
+          if (numEmbeddingCalls >= 3000) {
+            const now = Date.now();
+            const timePassed = now - startTime;
+            if (timePassed < 60000) {
+              console.log(
+                `Limit reached. Waiting for ${60000 - timePassed}ms.`
+              );
+              await new Promise((resolve) =>
+                setTimeout(resolve, 60000 - timePassed)
+              );
+            }
+            numEmbeddingCalls = 0;
+            startTime = Date.now();
+          }
 
-      const [responseData] = embeddingResponse.data;
+          const embeddingResponse = await openAI.embeddings.create({
+            model: 'text-embedding-ada-002',
+            input: chunk,
+          });
+          numEmbeddingCalls += 1;
 
-      const { data, error } = await supabase
-        .from('Application_Embeddings')
-        .insert({
-          application_id: applicationData[0].id,
-          embedding: responseData.embedding,
-          token_count: embeddingResponse.usage.total_tokens,
-        })
-        .select('*');
+          const [responseData] = embeddingResponse.data;
+          const { data, error } = await supabase
+            .from('Application_Embeddings')
+            .insert({
+              application_id: applicationData[0].id,
+              embedding: responseData.embedding,
+              token_count: embeddingResponse.usage.total_tokens,
+              content: applicationText,
+            })
+            .select('*');
+        }
+      }
     }
   });
 };
