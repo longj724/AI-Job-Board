7c770551e650f23f26bd1e5577f82ca04f37d18a
longj724 <justinlong596@gmail.com>
2024-05-11 18:57:07 -0400

    File meant for previous commit

--- newPipeline/utils/getJobInfoFromGHApplication.ts
+++ newPipeline/utils/getJobInfoFromGHApplication.ts
@@ -5,13 +5,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Relative Dependencies
-// import { supabaseClient } from './supabaseClient';
-// import { openAI } from './openAIClient';
+import { db } from '../prismaClient';
 
 export const getJobInfoFromGreenhouseApplications = async (): Promise<void> => {
-  let newCompaniesAdded = new Set();
-  let numEmbeddingCalls = 0;
-  let startTime = Date.now();
   const files = fs.readdirSync('./activeData/applications/gh');
 
   for (const file of files) {
@@ -22,82 +18,23 @@ export const getJobInfoFromGreenhouseApplications = async (): Promise<void> => {
     const $ = cheerio.load(content);
 
     const companyName = $('.company-name').text().replace('at', '').trim();
-    const jobTitle = $('.app-title').text();
-    const location = $('.location').text();
+    const jobTitle = $('.app-title').text().trim();
+    const location = $('.location').text().trim();
 
-    console.log('companyName is', companyName);
-    console.log('jobTitle is', jobTitle);
-    console.log('location is', location);
+    console.log('companyInfo is', {
+      companyName,
+      jobTitle,
+      location,
+      boardType: 'Greenhouse',
+    });
 
-    // if (newCompaniesAdded.has(companyName)) {
-    //   newCompaniesAdded.add(companyName);
-    //   const applicationDivs = $('#content p, #content span, #content ul');
-
-    //   let applicationText = '';
-    //   let applicationChunks: string[] = [];
-    //   applicationDivs.each(function (this: any) {
-    //     const text = $(this).text().trim();
-    //     applicationText += text;
-    //     if (text.split(' ').length > 7) {
-    //       applicationChunks.push(text);
-    //     }
-    //   });
-
-    // Put application into db
-    // if (
-    //   companyName !== '' &&
-    //   jobTitle !== '' &&
-    //   applicationChunks.length > 0
-    // ) {
-    //   const { data: applicationData, error: appError } = await supabase
-    //     .from('Applications')
-    //     .insert({
-    //       company: companyName,
-    //       title: jobTitle.trim(),
-    //       location: location,
-    //       content: applicationText,
-    //     })
-    //     .select('*');
-    //   console.log('appData is', applicationData);
-    //   console.log('chunks are', applicationChunks);
-
-    //   if (applicationData !== null) {
-    //     for (const chunk of applicationChunks) {
-    //       if (numEmbeddingCalls >= 3000) {
-    //         const now = Date.now();
-    //         const timePassed = now - startTime;
-    //         if (timePassed < 60000) {
-    //           console.log(
-    //             `Limit reached. Waiting for ${60000 - timePassed}ms.`
-    //           );
-    //           await new Promise((resolve) =>
-    //             setTimeout(resolve, 60000 - timePassed)
-    //           );
-    //         }
-    //         numEmbeddingCalls = 0;
-    //         startTime = Date.now();
-    //       }
-
-    //       const embeddingResponse = await openAI.embeddings.create({
-    //         model: 'text-embedding-ada-002',
-    //         input: chunk,
-    //       });
-    //       numEmbeddingCalls += 1;
-
-    //       const [responseData] = embeddingResponse.data;
-    //       const { data, error } = await supabase
-    //         .from('Application_Embeddings')
-    //         .insert({
-    //           application_id: applicationData[0].id,
-    //           embedding: JSON.stringify(responseData.embedding),
-    //           token_count: embeddingResponse.usage.total_tokens,
-    //           content: applicationText,
-    //         })
-    //         .select('*');
-    //     }
-    //   }
-    // }
-    // }
+    await db.posting.create({
+      data: {
+        company: companyName,
+        job_title: jobTitle,
+        location: location,
+      },
+    });
   }
 };
 
